8be78e5246d6b34aa6e2831b001e7d850e87de4f
Aaron Mandeville <63524772+aemandev@users.noreply.github.com>
2021-10-29 00:49:36 -0700

    Add files via upload

--- airfoilClass.m
+++ airfoilClass.m
@@ -5,21 +5,22 @@ classdef airfoilClass < handle
     %%%%%%%%%%%%%%%%%%%%%%%%%% ANGLE PHI MAYBE NOT GETTING CALCULATED
     %%%%%%%%%%%%%%%%%%%%%%%%%% CORRECTLY
     properties
-        Datapoints
-        Collocation ={};
-        Endpoints = {};
-        NormPanels = {};
+        xz = struct('Coords',{},'Collocation',{},'Endpoints',{},'NormPanels',{});
+        panelCoords  = struct('Coords',{},'Collocation',{},...
+            'Endpoints',{},'NormPanels',{});
         NumPanels
         FoilName
         FigHandle
         Betas
-   
-        
+        RefLength
+        Phi
+        PanelSize   
     end
     properties (Dependent)
         Phis
         BetaDeg
         PanelAngles
+        
     end
     
     methods
@@ -30,59 +31,112 @@ classdef airfoilClass < handle
             deg = rad2deg(obj.Betas);
         end
         function theta = get.PanelAngles(obj)
-            x_endpoints = obj.Endpoints.x{1};
-            z_endpoints = obj.Endpoints.z{1};
+            x_endpoints = obj.xz.Endpoints.x;
+            z_endpoints = obj.xz.Endpoints.z;
             for j = 1:length(x_endpoints)-1
                 dx = x_endpoints(j+1)-x_endpoints(j);
                 dz = z_endpoints(j+1)-z_endpoints(j);
                 theta(j) = atan2(dz,dx);
             end
+        end    
+        % Rotate panel by alpha radians
+        function coords = panel2xz(obj,alpha)
+            xz_coords = obj.xz.Coords;
+            rotMat = [cos(alpha) sin(alpha);
+                -sin(alpha) cos(alpha)];
+            coords = rotMat*xz_coords'; 
+            obj.panelCoords(1).Coords = coords';
+            obj.panelCoords.Collocation = rotMat*[obj.xz.Collocation.x;...
+                obj.xz.Collocation.z];
+            obj.panelCoords.Endpoints = rotMat*[obj.xz.Endpoints.x;...
+                obj.xz.Endpoints.z];
+            obj.panelCoords.NormPanels = rotMat*[obj.xz.NormPanels.x;...
+                obj.xz.NormPanels.z]';
         end
+        % Not really working correctly
+        function coords = xz2panel(obj,alpha)
+            xz_coords = obj.xz.Coords;
+            rotMat = [cos(alpha) -sin(alpha);
+                sin(alpha) cos(alpha)];
             
-        function obj = airfoilClass(foilName,foilPoints)
+            for i = 1:length(xz_coords)-1
+                xVec(i) = xz_coords(i+1,1)- xz_coords(i,1);
+                zVec(i) = xz_coords(i+1,2)- xz_coords(i,2);
+            end
+            
+            coords = rotMat*[xVec;zVec]; 
+            coords = coords';
+%             obj.panelCoords(1).Coords = coords';
+%             obj.panelCoords.Collocation = rotMat*[obj.xz.Collocation.x;...
+%                 obj.xz.Collocation.z];
+%             obj.panelCoords.Endpoints = rotMat*[obj.xz.Endpoints.x{:};...
+%                 obj.xz.Endpoints.z{:}];
+%             obj.panelCoords.NormPanels = rotMat*[obj.xz.NormPanels.x;...
+%                 obj.xz.NormPanels.z]';
+        end
+        
+        % Initialization good
+        function obj = airfoilClass(foilName,foilPoints,reflength)
             %airfoilClass Construct an instance of this class
             %   Detailed explanation goes here
             obj.FoilName = foilName;
-            obj.Datapoints = foilPoints;
+            obj.RefLength = reflength;
+            foilpointsClockwise = zeros(length(foilPoints),1);
+            foilpointsClockwise(:,1) = flipud(foilPoints(:,1));
+            foilpointsClockwise(:,2) = flipud(foilPoints(:,2));
+
+            obj.xz(1).Coords = foilpointsClockwise;      
         end
+ 
+        % Discretization good
         function obj = discretize(obj,numPanels)
             obj.NumPanels = numPanels;
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
-            xVec = obj.Datapoints(:,1);
-            zVec = obj.Datapoints(:,2);
+            if length(obj.xz.Coords(:,1)) < obj.NumPanels
+                fprintf(['Number of panels requested (',num2str(obj.NumPanels),...
+                    ') is greater than\nthe number of airfoil nodes available (',...
+                    num2str(length(obj.xz.Coords)),').\n\n']);
+                fprintf(['Mesh will be initialized using the max number of panels (',...
+                    num2str(length(obj.xz.Coords)),').\n\n']);
+            end
+            xVec = obj.xz.Coords(:,1);
+            zVec = obj.xz.Coords(:,2);
             panelSize = ceil(length(xVec)/numPanels);
             count = 1;
-            endPoints.x = {};
-            endPoints.z = {};
+            Endpoints.x = [];
+            Endpoints.z = [];
             
-            for i = 1:panelSize:length(xVec)
-                endPoints.x{1}(count) = xVec(i);
-                endPoints.z{1}(count) = zVec(i);
+            for i = 1:panelSize:length(xVec)-1
+                Endpoints.x(count) = xVec(i);
+                Endpoints.z(count) = zVec(i);
                 count = count+1;
             end
-            endPoints.x{1}(end+1) = xVec(end);
-            endPoints.z{1}(end+1) = zVec(end);
-            obj.Endpoints.x = endPoints.x;
-            obj.Endpoints.z = endPoints.z;
+            Endpoints.x(end+1) = xVec(end);
+            Endpoints.z(end+1) = zVec(end);
+            obj.xz.Endpoints.x = Endpoints.x;
+            obj.xz.Endpoints.z = Endpoints.z;
             
-            for j = 1:length(endPoints.x{1})-1
-                obj.Collocation.x(j) = mean([obj.Endpoints.x{1}(j) obj.Endpoints.x{1}(j+1)]);
-                obj.Collocation.z(j) = mean([obj.Endpoints.z{1}(j) obj.Endpoints.z{1}(j+1)]);
+            for j = 1:length(Endpoints.x)-1
+                obj.xz.Collocation.x(j) = mean([obj.xz.Endpoints.x(j) obj.xz.Endpoints.x(j+1)]);
+                obj.xz.Collocation.z(j) = mean([obj.xz.Endpoints.z(j) obj.xz.Endpoints.z(j+1)]);
                             
-                dx = (obj.Endpoints.x{1}(j+1)-obj.Endpoints.x{1}(j));
-                dz = (obj.Endpoints.z{1}(j+1)-obj.Endpoints.z{1}(j));
-                x = obj.Collocation.x(j);
-                normal = (dx/dz)*x+obj.Collocation.z(j);
-                normVecx{j} = [obj.Collocation.x(j) obj.Collocation.x(j)+dz/3];
-                normVecz{j} = [obj.Collocation.z(j) obj.Collocation.z(j)-dx/3];
+                dx = (obj.xz.Endpoints.x(j+1)-obj.xz.Endpoints.x(j));
+                dz = (obj.xz.Endpoints.z(j+1)-obj.xz.Endpoints.z(j));
+                obj.PanelSize(j) = (dx^2+dz^2)^.5;
+                x = obj.xz.Collocation.x(j);
+                normal = (dx/dz)*x+obj.xz.Collocation.z(j);
+                normVecx{j} = [obj.xz.Collocation.x(j) obj.xz.Collocation.x(j)-dz/3];
+                normVecz{j} = [obj.xz.Collocation.z(j) obj.xz.Collocation.z(j)+dx/3];
                 
-                obj.NormPanels.x(j,:) = normVecx{j};
-                obj.NormPanels.z(j,:) = normVecz{j};
+                obj.xz.NormPanels.x(j,:) = normVecx{j};
+                obj.xz.NormPanels.z(j,:) = normVecz{j};
                 
-                phi = rad2deg(atan2((obj.Endpoints.z{1}(j+1)-obj.Endpoints.z{1}(j)),...
-                    (obj.Endpoints.x{1}(j+1)-obj.Endpoints.z{1}(j))));
-
+                phi(j) = atan2(dz,dx);
+                if (phi(j) < 0)                                                        % Make all panel angles positive [deg]
+                    phi(j) = phi(j) + 2*pi;
+                end
+                obj.Phi(j) = phi(j);
             end
             
 
@@ -92,7 +146,7 @@ classdef airfoilClass < handle
         end
         function beta = calcBetas(obj,velocityDir,plotVel)
             normVec = obj.NormPanels;
-            CO = obj.Collocation;
+            CO = obj.xz.Collocation;
 
             if plotVel == 1
                 fig = obj.FigHandle;
@@ -124,32 +178,60 @@ classdef airfoilClass < handle
             obj.Betas = angle;
         end
         
-        function fig = plotPanels(obj)
-            fig = figure();
-            plot(obj.Datapoints(:,1),obj.Datapoints(:,2),'r-','linewidth',3),hold on
-            plot(obj.Endpoints.x{1},obj.Endpoints.z{1},'g-','linewidth',2)
-            plot(obj.Collocation.x,obj.Collocation.z,'ko',...
-                'markerfacecolor','k','markersize',2)
+        % Plot works
+        function fig = plotPanels(obj,plotKey,varargin)
+           
+%             fig = figure();
+            ref_length = obj.RefLength;
+            if strcmp(plotKey,'xy') == 1
+                plot(obj.xz.Coords(:,1)./ref_length,obj.xz.Coords(:,2)./ref_length,'r-','linewidth',3),hold on
+                plot(obj.xz.Endpoints.x./ref_length,obj.xz.Endpoints.z./ref_length,'g-','linewidth',2)
+                plot(obj.xz.Collocation.x./ref_length,obj.xz.Collocation.z./ref_length,'ko',...
+                    'markerfacecolor','k','markersize',2)
 
-            title(obj.FoilName)
-            legend('Airfoil Points','Discretized Panels','Collocation','location',...
-                'best');
-            grid on
-            axis equal
-            fill(obj.Endpoints.x{1},obj.Endpoints.z{1},'g')
-            obj.FigHandle = fig;
+                title(obj.FoilName)
+                legend('Airfoil Points','Discretized Panels','Collocation','location',...
+                    'best');
+                grid on
+                if size(varargin,1) >= 1
+                    if strcmp(varargin{1},'equal') == 1
+                        axis equal
+                    end
+                end
+                fill(obj.xz.Endpoints.x./ref_length,obj.xz.Endpoints.z./ref_length,'g')
+%                 obj.FigHandle = fig;
+            end
+            
+            if strcmp(plotKey,'panelCoords') == 1
+                plot(obj.panelCoords.Coords(:,1),obj.panelCoords.Coords(:,2),'r-','linewidth',3),hold on
+                plot(obj.panelCoords.Endpoints(1,:),obj.panelCoords.Endpoints(2,:),'g-','linewidth',2)
+                plot(obj.panelCoords.Collocation(1,:),obj.panelCoords.Collocation(2,:),'ko',...
+                    'markerfacecolor','k','markersize',2)
+                title(obj.FoilName)
+                legend('Airfoil Points','Discretized Panels','Collocation','location',...
+                    'best');
+                grid on
+                if size(varargin,1) >= 1
+                    if strcmp(varargin{1},'equal') == 1
+                        axis equal
+                    end
+                end
+%                 axis equal
+                fill(obj.panelCoords.Endpoints(1,:),obj.panelCoords.Endpoints(2,:),'g')
+%                 obj.FigHandle = fig;
+            end
         end
         
+        % Plot normals works
         function plotNormals(obj)
-            fig = obj.FigHandle;
-            figure(fig)
-            for i = 1:length(obj.NormPanels.x)
-            plot(obj.NormPanels.x(i,:),obj.NormPanels.z(i,:),'k')
+%             fig = obj.FigHandle;
+%             figure(fig)
+            for i = 1:length(obj.xz.NormPanels.x)
+            plot(obj.xz.NormPanels.x(i,:)./obj.RefLength,obj.xz.NormPanels.z(i,:)./obj.RefLength,'k')
             legend off
             end
         end
         
-            
 
     end
 end
--- airfoilProgram.m
+++ airfoilProgram.m
@@ -40,6 +40,7 @@ constant strength source panels (note that this method won't produce lift
 but you can get a cp distribution).
 %}
 close all; clc
+clear CP A B DL TH VEL b EP CO
 
 if exist('xyAirfoilData','var') ~= 1
     
@@ -54,37 +55,131 @@ if exist('xyAirfoilData','var') ~= 1
     xyAirfoilData = readmatrix([foilPath '/' file],'NumHeaderLines',1);
 end
 
-
 %% Create Panels and Plot
-numPanels = 60;
-velDir = [1 0];
-NACA = airfoilClass(file,xyAirfoilData);
+ref_length = 1;
+NACA = airfoilClass(file,xyAirfoilData,ref_length);
+numPanels = 200;
 NACA.discretize(numPanels);
-plot = NACA.plotPanels;
+
+% Plot initial airfoil
+figure()
+NACA.plotPanels('xy','equal');
 NACA.plotNormals;
 
+% Angle of attack
+alpha = 10;
 
-%% Solve Vortex Panel Method THIS IS WHERE I LEFT OFF
-gamma = 1;
-[u,w] = VOR2DC(gamma,NACA);
+% Calculate CP, CL, M (pitching moment), and velocity
+[CP,CL,M,VEL] = VP(NACA,alpha);
 
-function [u,w] = VOR2DC(gamma,airfoilObj)
-coll_x = airfoilObj.Collocation.x;
-coll_y = airfoilObj.Collocation.x;
+% Plot CP
+figure()
+subplot(2,1,1)
 
-theta1 = atan2((z-z2),(x-x2));
-theta2 = atan2((z-z1),(x-x1));
-R2 = (x-x2)^2 + (z-z2)^2;
-R1 = (x-x1)^2+(z-z1)^2;
+% Plot the rotated airfoil by *alpha* radians
+NACA.panel2xz(deg2rad(alpha));
+NACA.plotPanels('panelCoords');
 
-for i = 1:length(coll_x)
-    for j = 1:length(coll_x)
-        up = (gamma/(2*pi))*(theta2-theta1);
-        wp = (-gamma/(4*pi))*log(R1/R2);
+
+
+% NACA.plotNormals
+subplot(2,1,2)
+NACA.plotPanels('panelCoords');
+
+% Plot CP values 
+plot(NACA.xz.Collocation.x,-CP,'bo'),grid on, grid minor
+% set(gca, 'YDir','reverse')
+% axis equal
+
+
+function [CP,CL,M0,VEL] = VP(airfoilObj,alpha)
+AL = deg2rad(alpha);
+M = length(airfoilObj.xz.Collocation.x);
+for i = 1:M
+    for j = 1:M
+
+        X2T = airfoilObj.xz.Endpoints.x(j+1)-airfoilObj.xz.Endpoints.x(j);
+        Z2T = airfoilObj.xz.Endpoints.z(j+1)-airfoilObj.xz.Endpoints.z(j);
+        XT = airfoilObj.xz.Collocation.x(i)-airfoilObj.xz.Endpoints.x(j);
+        ZT = airfoilObj.xz.Collocation.z(i)-airfoilObj.xz.Endpoints.z(j);
+ 
+        X2 = X2T*cos(airfoilObj.Phi(j))+Z2T*sin(airfoilObj.Phi(j));
+        Z2 = 0;
+        X = XT*cos(airfoilObj.Phi(j))+ZT*sin(airfoilObj.Phi(j));
+        Z = -XT*sin(airfoilObj.Phi(j))+ZT*cos(airfoilObj.Phi(j));
+
+        if i == 1
+            DL(j) = X2;
+        end
         
+        if i == j
+            up = 0.5;
+            wp = 0;
+        else
+            [up,wp] = VOR2DC(X,Z,X2,Z2);
+        end
+
+        U = up*cos(-airfoilObj.Phi(j))+wp*sin(-airfoilObj.Phi(j));
+        W = -up*sin(-airfoilObj.Phi(j))+wp*cos(-airfoilObj.Phi(j));
+        
+        A(i,j) = -U*sin(airfoilObj.Phi(i))+W*cos(airfoilObj.Phi(i));
+        B(i,j) = U*cos(airfoilObj.Phi(i))+W*sin(airfoilObj.Phi(i));
     end
+
+%     b(i)= sin(airfoilObj.Phi(i)-AL);
+    b(i) = cos(AL)*sin(airfoilObj.Phi(i))-sin(AL)*cos(airfoilObj.Phi(i));
+    
 end
 
+% Apply Kutta B.C.
+b = b';
+A(M-1,:) = 0;
+A(M-1,M-1) = 1;
+A(M-1,1) = 1;
+b(M-1) = 0;
+
+% Solve for Gamma
+G = A\b;
+
+% Calculate V and Cp
+CL = 0;
+M0 = 0;
+Vinf = 1;
+for i = 2:M
+    TEMP = 0;
+    for j = 1:M
+        TEMP=TEMP+B(i,j)*G(j);
+    end
+    VEL(i)=TEMP+cos(AL)*cos(airfoilObj.Phi(i))+sin(AL)*sin(airfoilObj.Phi(i));
+    CL=CL+VEL(i)*airfoilObj.PanelSize(i);
+    CP(i) = 1-((VEL(i)+VEL(i-1))/2/Vinf)^2;
+    
+% %     M0 = M0 + VEL(i)*DL(i)*airfoilObj.xz.Collocation.x(i)*cos(airfoilObj.Phi(i));
+    
 end
+M0 = sum(CP.*airfoilObj.xz.Collocation.x.*airfoilObj.PanelSize.*cos(airfoilObj.Phi));
+fprintf(['Results with ',num2str(length(airfoilObj.Phi)),' panels at ',num2str(alpha),...
+    ' degrees angle of ','attack:\nCL = ',num2str(CL),'\n'])
+end
+
+function [up,wp] = VOR2DC(X,Z,X2,Z2)
+% 
+% R1 = X^2+Z^2;
+% R2 = (X-X2)^2+Z^2;
+% TH1 = atan2(Z,X);
+% TH2 = atan2(Z-Z2,X-X2);
+
+R1 = sqrt(X^2+Z^2);
+R2 = sqrt((X-X2)^2+Z^2);
+TH1 = atan2(Z,X);
+TH2 = atan2(Z,X-X2);
+
+
+up = (1/(2*pi))*(TH2-TH1);
+wp = (1/(2*pi))*log(R2/R1);
+
+
+end
+
 
 
